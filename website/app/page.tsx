03400f954e7f6b85f2caab7ec54f28f704d72082
marcusanthonylorenzo <marcus.anthony.lorenzo@gmail.com>
2025-02-12 10:47:11 -0800

    configure shadcn no css var

---
+++ website/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@shadcn/ui';
+
+export default function Home() {
+  return (
+    <>
+      <div id={`mainpage-Container`}>
+        <Button>
+          { "ShadCN Test Btn"}
+        </Button>
+      </div>
+    </>
+  );
+}
